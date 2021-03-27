f6c99f1b9163c66131133dd4da9f2607a73cf4c9
naitikkumar <r.naitikkumar1992@gmail.com>
2021-03-27 15:20:21 +0530

    Create create_attach_dsk.ps1

---
+++ create_attach_dsk.ps1
@@ -0,0 +1,15 @@
+param(
+$rgName,
+$vmName,
+$location,
+$storageType
+)
+$dataDiskName = $vmName + '_datadisk1'
+
+$diskConfig = New-AzDiskConfig -SkuName $storageType -Location $location -CreateOption Empty -DiskSizeGB 128
+$dataDisk1 = New-AzDisk -DiskName $dataDiskName -Disk $diskConfig -ResourceGroupName $rgName
+
+$vm = Get-AzVM -Name $vmName -ResourceGroupName $rgName 
+$vm = Add-AzVMDataDisk -VM $vm -Name $dataDiskName -CreateOption Attach -ManagedDiskId $dataDisk1.Id -Lun 1
+
+Update-AzVM -VM $vm -ResourceGroupName $rgName
