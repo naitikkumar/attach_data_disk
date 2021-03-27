0c5b6826e8edb252f0696bcd3091c1e9a0034fb3
naitikkumar <r.naitikkumar1992@gmail.com>
2021-03-27 15:24:10 +0530

    Create int_datadsk_cse.ps1

---
+++ int_datadsk_cse.ps1
@@ -0,0 +1,6 @@
+param(
+$location,
+$scriptName,
+$fileName
+)
+Set-AzVMCustomScriptExtension -ResourceGroupName $rgName -Location $locName -VMName $vmName -Name $scriptName -TypeHandlerVersion "1.4" -StorageAccountName "mystore1" -StorageAccountKey "primary-key" -FileName $fileName -ContainerName "scripts"
